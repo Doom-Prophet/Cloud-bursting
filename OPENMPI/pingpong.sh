2c501e4860b0bf774548af1e67376a2566e8dbdb
Zicheng Ma <zicheng5@illinois.edu>
2023-10-13 05:43:37 -0500

    test

--- OPENMPI/pingpong.sh
+++ OPENMPI/pingpong.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pingpong_test
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
 #SBATCH --time=4
 #SBATCH --output=pingpong_output.txt
 #SBATCH --partition=cpu_mini
