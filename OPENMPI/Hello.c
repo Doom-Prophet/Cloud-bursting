f87779f651de4db9e3373abd75c78ef80d7919cc
Zicheng Ma <zicheng5@illinois.edu>
2023-10-13 05:52:46 -0500

    test

--- OPENMPI/Hello.c
+++ OPENMPI/Hello.c
@@ -1,7 +1,10 @@
+#include <mpi.h>
 #include <stdio.h>
 
-int main(){
-    printf("Hello World!");
-
-    return 0;    
+int main(int argc, char** argv) {
+    MPI_Init(NULL, NULL);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    printf("Hello from process %d\n", world_rank);
+    MPI_Finalize();
 }
