15d75e5385352b6c2b279203f6ff4ebc8b60b816
Zicheng Ma <zicheng5@illinois.edu>
2023-10-13 06:21:11 -0500

    test

--- OPENMPI/pingpong.c
+++ OPENMPI/pingpong.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
   if (nproc != 2) {
-    printf("nproc:",nproc);
+    printf("MPI_COMM_WORLD:",MPI_COMM_WORLD);
     printf("me:",me);
     if (me == 0) printf("This benchmark should be run on exactly two processes");
     MPI_Abort(MPI_COMM_WORLD, 1);
