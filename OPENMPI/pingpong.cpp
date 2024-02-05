ba953e1ad633bcc07ef0a478423c56fc5c9cabc3
Zicheng Ma <zicheng5@illinois.edu>
2024-02-05 07:07:10 -0600

    test pingpong

--- OPENMPI/pingpong.cpp
+++ OPENMPI/pingpong.cpp
@@ -20,7 +20,7 @@ public:
 
     void Ping() {
         ping_count++;
-        if (ping_count < 5) {
+        if (ping_count < 2) {
             std::cout << "Ping from rank " << rank << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(1));
             partner.Task(&PingPong::Pong).Remote();
@@ -37,8 +37,8 @@ public:
     }
 
     void Test() {
-        std::cout << "Checkpoint 1 " << rank << std::endl;
-        printf("Checkpoint 2");
+        std::cout << "Checkpoint 4 " << rank << std::endl;
+        printf("Checkpoint 5");
     }
 };
 
@@ -46,20 +46,30 @@ PingPong *CreatePlayer(int rank_input){
         return new PingPong(rank_input);
     }
 
-RAY_REMOTE(CreatePlayer, &PingPong::RegisterPartner, &PingPong::Ping, &PingPong::Pong);
+RAY_REMOTE(CreatePlayer, &PingPong::RegisterPartner, &PingPong::Ping, &PingPong::Pong, &PingPong::Test);
 
 int main(int argc, char **argv) {
     ray::Init();
 
+    printf("Checkpoint 1");
+
     ray::ActorHandle<PingPong> alice = ray::Actor(CreatePlayer).Remote(1);
     ray::ActorHandle<PingPong> bob = ray::Actor(CreatePlayer).Remote(2);
+
+    printf("Checkpoint 2");
 
     alice.Task(&PingPong::RegisterPartner).Remote(bob);
     bob.Task(&PingPong::RegisterPartner).Remote(alice);
 
+    printf("Checkpoint 3");
+
+    alice.Task(&PingPong::Test).Remote();
+
+    printf("Checkpoint 6");
+
     alice.Task(&PingPong::Ping).Remote();
 
-    printf("Checkpoint 3");
+    printf("Checkpoint 7");
 
     // ray::Shutdown();
     return 0;
