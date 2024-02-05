93fda2e63c7673116f05e59a0c9ba8ab84a775e1
Zicheng Ma <zicheng5@illinois.edu>
2024-02-05 05:27:56 -0600

    test pingpong

--- OPENMPI/pingpong.cpp
+++ OPENMPI/pingpong.cpp
@@ -5,43 +5,45 @@
 
 class PingPong {
 public:
-    int ping_count;
-    int rank;
-    ray::ActorHandle<PingPong> partner;
+    static int ping_count;
+    static int rank;
+    static ray::ActorHandle<PingPong> partner;
 
-    PingPong(int rank) : ping_count(0), rank(rank) {}
+    PingPong(int rank) { PingPong::rank = rank; }
 
-    void RegisterPartner(ray::ActorHandle<PingPong> partner) {
-        this->partner = partner;
+    static void RegisterPartner(ray::ActorHandle<PingPong> partner) {
+        PingPong::partner = partner;
     }
 
-    ray::ObjectRef<void> Ping() {
+    static ray::ObjectRef<void> Ping() {
         ping_count++;
         if (ping_count < 5) {
             std::cout << "Ping from rank " << rank << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            partner.Task(&PingPong::Pong).Remote();
+            return partner.Task(&PingPong::Pong).Remote();
         }
-        return ray::ObjectRef<void>();  // Return an empty object ref.
+        return ray::ObjectRef<void>();
     }
 
-    ray::ObjectRef<void> Pong() {
+    static ray::ObjectRef<void> Pong() {
         std::cout << "Pong from rank " << rank << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        partner.Task(&PingPong::Ping).Remote();
-        return ray::ObjectRef<void>();  // Return an empty object ref.
+        return partner.Task(&PingPong::Ping).Remote();
     }
 };
+
+// Initialize static members
+int PingPong::ping_count = 0;
+int PingPong::rank = 0;
+ray::ActorHandle<PingPong> PingPong::partner;
 
 RAY_REMOTE(PingPong::RegisterPartner, PingPong::Ping, PingPong::Pong);
 
 int main(int argc, char **argv) {
-    ray::RayConfig config;
-    config.local_mode = true;
-    ray::Init(config);
+    ray::Init();
 
-    auto alice = ray::ActorRemote(PingPong::Factory, 1);
-    auto bob = ray::ActorRemote(PingPong::Factory, 2);
+    auto alice = ray::Actor(PingPong::Factory, 1).Remote();
+    auto bob = ray::Actor(PingPong::Factory, 2).Remote();
 
     alice.Task(&PingPong::RegisterPartner, bob).Remote();
     bob.Task(&PingPong::RegisterPartner, alice).Remote();
