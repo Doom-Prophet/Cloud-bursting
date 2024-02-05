41e91870f49c65e80753b1bcc493714040facd6c
Zicheng Ma <zicheng5@illinois.edu>
2024-02-05 14:36:10 -0600

    test

--- OPENMPI/pingpong.cpp
+++ OPENMPI/pingpong.cpp
@@ -106,7 +106,7 @@ int main(){
     std::cout << "Hi 2" << std::endl;
     // Call the actor's remote function
     auto result = actor.Task(&Counter::Add).Remote(1);
-    int value = *res.Get(); // Retrieve the result of the remote function call
+    int value = *result.Get(); // Retrieve the result of the remote function call
     // printf("%d\n", value); // Correctly print the integer result
 
     std::cout << "Hi 3" << std::endl;
