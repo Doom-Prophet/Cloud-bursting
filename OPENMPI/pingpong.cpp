e89a2662851e08efc9568877df63a859c5cd44e4
Zicheng Ma <zicheng5@illinois.edu>
2024-02-14 03:48:31 -0600

    test 2.14

--- OPENMPI/pingpong.cpp
+++ OPENMPI/pingpong.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <ray/api.h>
-#include <thread>
-#include <chrono>
+// #include <iostream>
+// #include <ray/api.h>
+// #include <thread>
+// #include <chrono>
 
 // class PingPong {
 //     int ping_count = 0;
@@ -85,37 +85,62 @@
 //     return 0;
 // }
 
+/// This is an example of Ray C++ application. Please visit
+/// `https://docs.ray.io/en/master/ray-core/walkthrough.html#installation`
+/// for more details.
+
+/// including the `<ray/api.h>` header
+#include <ray/api.h>
+
+/// common function
+int Plus(int x, int y) { return x + y; }
+/// Declare remote function
+RAY_REMOTE(Plus);
+
+/// class
 class Counter {
-    int count;
-public:
-    Counter(int init) {count = init;}
-    int Add(int x) {
-        return x + 1;
-        std::cout << "Hi" << std::endl;
-    }
+ public:
+  int count;
+
+  Counter(int init) { count = init; }
+  /// static factory method
+  static Counter *FactoryCreate(int init) { return new Counter(init); }
+
+  /// non static function
+  int Add(int x) {
+    count += x;
+    return count;
+  }
 };
+/// Declare remote function
+RAY_REMOTE(Counter::FactoryCreate, &Counter::Add);
 
-Counter *CreateCounter(int init) {
-    return new Counter(init);
-}
-RAY_REMOTE(CreateCounter, &Counter::Add);
+int main(int argc, char **argv) {
+  /// initialization
+  ray::Init();
 
-int main(){
-    ray::Init();
+  /// put and get object
+  auto object = ray::Put(100);
+  auto put_get_result = *(ray::Get(object));
+  std::cout << "put_get_result = " << put_get_result << std::endl;
 
-    std::cout << "Hi 1" << std::endl;
-    // Create a actor
-    ray::ActorHandle<Counter> actor = ray::Actor(CreateCounter).Remote(0);
+  /// common task
+  auto task_object = ray::Task(Plus).Remote(1, 2);
+  int task_result = *(ray::Get(task_object));
+  std::cout << "task_result = " << task_result << std::endl;
 
-    std::cout << "Hi 2" << std::endl;
-    // Call the actor's remote function
-    auto result = actor.Task(&Counter::Add).Remote(1);
-    // EXPECT_EQ(1, *(ray::Get(result)));
-    int value = *result.Get(); // Retrieve the result of the remote function call
-    printf("%d\n", value); // Correctly print the integer result
+  /// actor
+  ray::ActorHandle<Counter> actor = ray::Actor(Counter::FactoryCreate).Remote(0);
+  /// actor task
+  auto actor_object = actor.Task(&Counter::Add).Remote(3);
+  int actor_task_result = *(ray::Get(actor_object));
+  std::cout << "actor_task_result = " << actor_task_result << std::endl;
+  /// actor task with reference argument
+  auto actor_object2 = actor.Task(&Counter::Add).Remote(task_object);
+  int actor_task_result2 = *(ray::Get(actor_object2));
+  std::cout << "actor_task_result2 = " << actor_task_result2 << std::endl;
 
-    std::cout << "Hi 3" << std::endl;
-
-    ray::Shutdown();
-    return 0;
+  /// shutdown
+  ray::Shutdown();
+  return 0;
 }
