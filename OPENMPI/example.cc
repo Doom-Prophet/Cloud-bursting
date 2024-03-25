e5e08f873f78d1361dc9e5a3a8d552b8147cb073
Zicheng Ma <zicheng5@illinois.edu>
2024-03-25 13:51:52 -0500

    3.25.2

--- OPENMPI/example.cc
+++ OPENMPI/example.cc
@@ -107,9 +107,13 @@ int main(int argc, char **argv) {
   auto test_obj = actor.Task(&Counter::test_put).Remote(3);
   std::cout << "Test 5:" << typeid(test_obj).name() << std::endl;
 
-  auto obj = *(ray::Get(test_obj));
+  auto obj = ray::Get(test_obj);
   std::cout << "Test 9:" << typeid(obj).name() << std::endl;
   std::cout << "Test 10:" << obj << std::endl;
+
+  // auto obj = *(ray::Get(test_obj));
+  // std::cout << "Test 9:" << typeid(obj).name() << std::endl;
+  // std::cout << "Test 10:" << obj << std::endl;
 
   // obj_refs_int.push_back(test_obj);
 
